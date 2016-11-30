a2a0bece23b67720b472022b8ed04706810512a7
conor <conorbudge555@gmail.com>
2016-11-30 12:01:30 -0600

    Pulled the JSON from Github, and connected to load the object in the HTML

--- SuperHeroes.js
+++ SuperHeroes.js
@@ -1,50 +1,60 @@
 /**
  * Created by conor on 11/29/16.
  */
+function heroes() {
+    let header = document.querySelector('header');
+    let section = document.querySelector('section');
 
+//Loading the JSON to be used on the page
+    let requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+    let request = new XMLHttpRequest();
 
+//Change the first parameter to set a different method, GET, POST, PUT, DELETE
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
 
+    request.onload = function () {
+        let superHeroes = request.response;
+        populateHeader(superHeroes);
+        showHeroes(superHeroes);
+    }
 
+    function populateHeader(jsonObj) {
+        let myH1 = document.createElement('h1');
+        myH1.textContent = jsonObj['squadName'];
+        header.appendChild(myH1);
 
+        let myPara = document.createElement('p');
+        myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
+        header.appendChild(myPara);
+    }
 
-let superHeroes = {
-    "squadName" : "Super hero squad",
-    "homeTown" : "Metro City",
-    "formed" : 2016,
-    "secretBase" : "Super tower",
-    "active" : true,
-    "members" : [
-        {
-            "name" : "Molecule Man",
-            "age" : 29,
-            "secretIdentity" : "Dan Jukes",
-            "powers" : [
-                "Radiation resistance",
-                "Turning tiny",
-                "Radiation blast"
-            ]
-        },
-        {
-            "name" : "Madame Uppercut",
-            "age" : 39,
-            "secretIdentity" : "Jane Wilson",
-            "powers" : [
-                "Million tonne punch",
-                "Damage resistance",
-                "Superhuman reflexes"
-            ]
-        },
-        {
-            "name" : "Eternal Flame",
-            "age" : 1000000,
-            "secretIdentity" : "Unknown",
-            "powers" : [
-                "Immortality",
-                "Heat Immunity",
-                "Inferno",
-                "Teleportation",
-                "Interdimensional travel"
-            ]
+    function showHeroes(jsonObj) {
+        let heroes = jsonObj['members'];
+        for (i = 0; i < heroes.length; i++) {
+            let myArticle = document.createElement('article');
+            let myH2 = document.createElement('h2');
+            let myPara1 = document.createElement('p');
+            let myPara2 = document.createElement('p');
+            let myPara3 = document.createElement('p');
+            let myList = document.createElement('ul');
+            myH2.textContent = heroes[i].name;
+            myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
+            myPara2.textContent = 'Age: ' + heroes[i].age;
+            myPara3.textContent = 'Superpowers:';
+            let superPowers = heroes[i].powers;
+            for (j = 0; j < superPowers.length; j++) {
+                let listItem = document.createElement('li');
+                listItem.textContent = superPowers[j];
+                myList.appendChild(listItem);
+            }
+            myArticle.appendChild(myH2);
+            myArticle.appendChild(myPara1);
+            myArticle.appendChild(myPara2);
+            myArticle.appendChild(myPara3);
+            myArticle.appendChild(myList);
+            section.appendChild(myArticle);
         }
-    ]
+    }
 }
